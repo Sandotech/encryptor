6f4774c4d5e3e0d757ddccdd5773f94035630c21
santodev <dseosantos76@gmail.com>
2023-07-30 13:58:16 -0400

    cracking ecryption function added

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -53,4 +53,14 @@ class Encryptor
     decrypt_file.write(decrypted_content)
     decrypt_file.close
   end
+
+  def supported_characters
+    (' '..'z').to_a
+  end
+
+  def crack(message)
+    supported_characters.count.times.collect do |attempt|
+      decrypt(message, attempt)
+    end
+  end
 end
